df394bea04d243aad8cb623e6b75e6c03f71fb37
Flutterneu <rsmx141@gmail.com>
2024-03-21 18:02:31 +0530

    nodemailer

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+let transporter = nodemailer.createTransport({
+  host: 'smtp-relay.brevo.com', // Your email provider's SMTP host
+  port: 587, // Typically 587 for secure SMTP
+  secure: false, // True for 465, false for other ports
+  auth: {
+    user: 'dannyanime367@gmail.com', // Your email
+    pass: 'k7w2UOGILPJjv8Tg', // Your email password or app-specific password
+  },
+});
+
+app.post('/send-email', (req, res) => {
+  const { to, subject, text } = req.body;
+  
+  let mailOptions = {
+    from: 'dannyanime367@gmail.com',
+    to: to,
+    subject: subject,
+    text: text,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send(error.toString());
+    }
+    res.status(200).send('Email sent: ' + info.response);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
