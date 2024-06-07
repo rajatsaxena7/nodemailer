1ebeb18443362ea573fcb1f2b209d715402b82c5
Roopa-21 <73734157+Roopa-21@users.noreply.github.com>
2024-06-07 13:37:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 const sendEmail = async (req, res, template) => {
-  const { to, roNumber, articleTitle, vendorName, vendorContact, attachmentUrl,notesheetNumber,amount} = req.body;
+  const { to, roNumber, articleTitle, vendorName, vendorContact, attachmentUrl,notesheetNumber,amount,dateOfApproval,advertisementNumber} = req.body;
 
   let transporter = nodemailer.createTransport({
     host: 'smtp.hostinger.com',
@@ -34,7 +34,6 @@ const sendEmail = async (req, res, template) => {
 I hope this email finds you well.
 
 Please find attached the Release Order ${roNumber} for the article that is scheduled for publication. Please review the attached Release Order and approve/reject it from your dashboard for publication. Your prompt attention to this matter is appreciated to ensure timely publication.
-
 For any further clarifications or modifications, please feel free to reach out to us. Thank you for your cooperation.
 
 Best regards,
@@ -50,7 +49,6 @@ case 'billRaisedDeputy':
 I hope this email finds you well.
 
 This is to inform you that a bill has been raised by ${vendorName} for the publication of the article bearing RO Number ${roNumber}.
-
 Please review the bill and process it accordingly. You can view the details and approve/reject the bill from your dashboard.
 
 Thank you for your prompt attention to this matter.
@@ -116,11 +114,138 @@ Thank you for your prompt attention to this matter.
 
 Best regards,
 Assistant
-contact@gmail.com
 Department of Information and Public Relations
 Government of Arunachal Pradesh`;
       break;
 
+
+ case 'faoNotesheet':
+      mailOptions.subject = `Review Request for Notesheet ${notesheetNumber} for Release Order ${roNumber}`;
+      mailOptions.text = `Dear FAO,
+
+I hope this email finds you well.
+
+This is to inform you that a notesheet has been created with Notesheet No. ${notesheetNumber} and an amount of ${amount} for the Release Order ${roNumber}.
+Please review the notesheet and provide your response by either adding your notes or rejecting it. You can view the details and approve or reject the bill from your dashboard.
+
+Thank you for your prompt attention to this matter.
+
+Best regards,
+Deputy Director
+Department of Information and Public Relations
+Government of Arunachal Pradesh`;
+      break;
+      
+ case 'directorNotesheet':
+      mailOptions.subject = `Review and Approval Request for Notesheet ${notesheetNumber}`;
+      mailOptions.text = `Dear Director,
+
+I hope this email finds you well.
+
+This is to inform you that a notesheet has been created with Notesheet No. ${notesheetNumber} and an associated amount of ${amount} pertaining to the Release Order ${roNumber}.
+Please review the notesheet and provide your response by either adding your notes or rejecting it. You can view the details and approve or reject the notesheet from your dashboard.
+
+Thank you for your prompt attention to this matter.
+
+Best regards,
+FAO
+Department of Information and Public Relations
+Government of Arunachal Pradesh`;
+      break;
+
+
+
+   case 'secretaryNotesheet':
+      mailOptions.subject = `Approval Needed: Notesheet ${notesheetNumber}`;
+      mailOptions.text = `Dear Secretary,
+
+I hope this email finds you well.
+
+A notesheet with No. ${notesheetNumber} and amount [amount] related to Release Order ${roNumber} requires your approval.
+Please review the details and indicate your decision via your dashboard.
+
+Thank you for your prompt attention to this matter.
+
+Best regards,
+Director
+Department of Information and Public Relations
+Government of Arunachal Pradesh`;
+      break;
+
+
+ case 'uploadSanction':
+      mailOptions.subject = `Action Required: Upload Sanction Letter for Notesheet ${notesheetNumber}`;
+      mailOptions.text = `Dear Assistant,
+
+I hope this email finds you well.
+
+This is to inform you that the Notesheet ${notesheetNumber} for the article has been accepted.
+Please view the details and upload the sanction letter if approved. You can either approve or reject it from your dashboard.
+
+Thank you for your prompt attention to this matter.
+
+Best regards,
+Secretary
+Department of Information and Public Relations
+Government of Arunachal Pradesh`;
+      break;
+
+
+ case 'approvalSanction':
+      mailOptions.subject = `Action Required: View and Submit Approval for Sanction letter`;
+      mailOptions.text = `Dear Accountant,
+
+I hope this email finds you well.
+
+This is to inform you that the Sanction letter for the article has been uploaded.
+Please view the details and, if approved, submit the sanction letter. You can either approve or reject it from your dashboard.
+
+Thank you for your prompt attention to this matter.
+
+Best regards,
+Assistant
+Department of Information and Public Relations
+Government of Arunachal Pradesh`;
+      break;
+
+    
+    
+    case 'approvedTFao':
+      mailOptions.subject = ` Approval Notification for Notesheet ${notesheetNumber} `;
+      mailOptions.text = `Dear FAO,
+
+I hope this email finds you well.
+
+
+This is to inform you that the notesheet with Notesheet No. ${notesheetNumber} has been approved on ${dateOfApproval}.
+
+Please review the approval details at your earliest convenience.
+
+Thank you for your prompt attention to this matter.
+
+Best regards,
+Accountant
+Department of Information and Public Relations
+Government of Arunachal Pradesh`;
+      break;
+
+
+ case 'approvedTCase':
+      mailOptions.subject = `Approval and Acknowledgement of Advertisement ${advertisementNumber}`;
+      mailOptions.text = `Dear Caseworker,
+
+I hope this email finds you well.
+
+This is to inform you that the advertisement with Release Order Number ${roNumber} has been approved and acknowledged by ${vendorName}.
+
+
+Thank you for your prompt attention to this matter.
+
+Best regards,
+Accountant
+Department of Information and Public Relations
+Government of Arunachal Pradesh`;
+      break;
       
       case 'vendorreply':
       mailOptions.subject = `  Acceptance of Release Order ${roNumber} by ${vendorName}`;
@@ -138,6 +263,12 @@ Department of Information and Public Relations
 Government of Arunachal Pradesh`;
       break;
 
+
+
+
+
+
+      
 
     case 'approval-request':
       mailOptions.subject = `Request for Approval - Release Order for ${articleTitle}`;
